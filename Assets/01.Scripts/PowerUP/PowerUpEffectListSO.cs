65796c2426ecb3b1701db3aabac561430d19cc2b
LEE SANG GYU <tkdrb062@gmail.com>
2024-05-21 14:25:23 +0900

    feat: PowerUpListSO

---
+++ Assets/01.Scripts/PowerUP/PowerUpEffectListSO.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "SO/Item/PowerUp/EffectList")]
+public class PowerUpEffectListSO : ScriptableObject
+{
+    public List<PowerUpEffectSO> list = new List<PowerUpEffectSO>();
+}
---
+++ Assets/01.Scripts/PowerUP/PowerUpListSO.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "SO/Item/PowerUp/List")]
+public class PowerUpListSO : ScriptableObject
+{
+    public List<PowerUpSO> list = new List<PowerUpSO>();
+}
